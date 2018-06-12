8ad058b7b4d009dd1ee178c43597e6c26979933f
UMRhamster <32890418+UMRhamster@users.noreply.github.com>
2018-06-12 11:29:04 +0800

    Initial commit

---
+++ app/src/main/java/com/whut/umrhamster/weatherforecast/View/FragmentWeather.java
@@ -0,0 +1,10 @@
+package com.whut.umrhamster.weatherforecast.View;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by 12421 on 2018/6/12.
+ */
+
+public class FragmentWeather extends Fragment {
+}
